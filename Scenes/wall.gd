30248eb63bfa5f42f68591771822697cd6a1863f
yashm <itsyashmistri@gmail.com>
2025-10-20 17:41:07 -0400

    fix shadows showing over sprites and start using normal maps for environment

---
+++ Scenes/wall.gd
@@ -0,0 +1,6 @@
+extends Sprite2D
+
+# todo have walls fade out if they would block player
+# ideas:
+# have every wall keep track of player position relative to them and fade out if y distance becomes negative
+# every time player steps on a tile hide walls below that tile
