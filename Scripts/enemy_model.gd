d3a709f70ef183832278e8219f21210d2fb34531
yashmistri <itsyashmistri@gmail.com>
2025-10-24 17:38:02 -0400

    change method of procedural animated walking
    uses vectors to determine when to move foot

--- Scripts/enemy_model.gd
+++ Scripts/enemy_model.gd
@@ -6,8 +6,8 @@ var max_stride_length :float = 1.2
 var stride_length:float = 1.1
 var stride_height:float = 1.0
 var max_rotation: float = PI/2
-var stride_time:float = 0.3
-var reset_time:float = 0.1
+var stride_time:float = 0.1
+var reset_time:float = 0.01
 var velocity:Vector3
 @export
 var curve:Curve3D
@@ -60,47 +60,72 @@ func _ready() -> void:
 #	instead of tween whole transform tween x,z,basis normally and tween y with parabola
 #	not possible so make path between start and end and tween that for position
 #walking state:
-#	foottrack node position changes based on movement vector
+#	tracker vector changes based on movement vector
 #	distance foot travels before triggering move also changes
-#		use vector from max drag before move to max stride vector
+#		get vector from vector base to foot and tracker vector
+#		move if dot product is negative
 #		vector changes with both move direction and rotation
 #		use dot product to determine when foot drags too far
 #		display debug vector first before implementing rest of function
 #		start with circle for vector orientations then do ellipse
+#		
 #	forward: 
+#rotate holder with move direction
+#change tracker vector based on state
 func move(foot : Node3D, foot_track : Node3D) -> void:
-	var vec: Vector3 = foot_track.global_position - foot.global_position
-	var d :float = vec.length()
+	var tracker_vec_origin = foot_track.get_node("Holder/Origin")
+	var tracker_vec_tip = foot_track.get_node("Holder/Tip")
+	var foot_vec: Vector3 = foot.global_position - tracker_vec_origin.global_position
+	var tracker_vec: Vector3 = tracker_vec_tip.global_position - tracker_vec_origin.global_position
+	var d :float = foot_vec.dot(tracker_vec)
 	var r :float = foot.global_basis.x.angle_to(foot_track.global_basis.x)
 	var move_foot:bool = false # if foot needs to move
-	var final_transform:Transform3D
-#	todo branch based on current state
-	if d > max_stride_length or r > max_rotation:
-		# length of this will be huge at start therefore keep to standard length
-		final_transform = Transform3D(foot_track.global_basis, foot_track.global_position + vec.limit_length(stride_length))
+	var final_pos: Vector3
+	if d < 0:
+		final_pos = tracker_vec_tip.global_position 
 		move_foot = true
 	if move_foot and not current_tween.is_valid():
 		
 		current_tween =get_tree().create_tween()
-		current_tween.tween_property(foot, "global_transform", final_transform, stride_time).set_ease(Tween.EASE_IN_OUT)
+		current_tween.tween_property(foot, "global_position", final_pos, stride_time).set_ease(Tween.EASE_IN_OUT)
 		
 		current_tween.tween_callback(current_tween.kill)
 
+#how to lean torso forwards and to the side of lead foot when foot is far forward
+#how to get lead foot:
+#	get 2d vector of feet releative to self forward
+#	lead foot is max dot product
+#get basis of torso to lead foot
 func move_torso():
+	var r_local_pos :Vector3= $RightFoot.global_position - global_position
+	var l_local_pos :Vector3= $LeftFoot.global_position - global_position
+	print("right {0} left {1}".format([r_local_pos, l_local_pos]))
+	if(r_local_pos.z < l_local_pos.z):
+		pass
+	
 	#dist between feet
 	#feet dist range is {0.2,2.2}
-	var d: float = $RightFoot.global_position.distance_to($LeftFoot.global_position)
+	var d: float = r_local_pos.distance_to(l_local_pos)
 	#print(d)
 	var default_y:= 0.0
 	var shift_range:= 0.5
 	var shift_amount := remap(d, 0.2, 2.2, default_y, default_y-shift_range)
 	$robot.position.y = shift_amount
-	
+
+
+#todo change stride time with speed
 func _process(delta: float) -> void:
 	velocity = $"..".velocity
 	#keep restarting until stop moving, then let timer finish
 	if velocity.length() > 0.1:
 		$Reset.start()
+		#rotate tracker vec holder to movement direction independent of look direction
+		$RightFootTrack/Holder.rotation.y = Vector3.FORWARD.signed_angle_to(velocity,Vector3.UP) - rotation.y
+		$LeftFootTrack/Holder.rotation.y = Vector3.FORWARD.signed_angle_to(velocity,Vector3.UP) - rotation.y
+	else:
+		$RightFootTrack/Holder.rotation.y = 0
+		$LeftFootTrack/Holder.rotation.y = 0
+	
 	move($RightFoot,$RightFootTrack)
 	move($LeftFoot,$LeftFootTrack)
 	move_torso()
