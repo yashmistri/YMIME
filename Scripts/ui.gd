8d42ddd71df794dd47d45edbde3f12076bee7b2d
yashm <itsyashmistri@gmail.com>
2026-02-20 22:55:26 -0500

    make branch and clean up directory
    deleted files unnecessary for developing animation system

--- Scripts/ui.gd
+++
@@ -1,8 +0,0 @@
-extends CanvasLayer
-
-func _process(delta: float) -> void:
-	var player:Character = $"../.."
-	$Panel/HBoxContainer/HealthLabel.text = "HP: {0}/{1}".format([player.current_health, player.max_health])
-	$Panel/HBoxContainer/Charges.text = "Charges: {0}/{1}".format([player.ability1_charges, player.ability1_max_charges])
-	$Panel/HBoxContainer/Level.text = "Player Level: {0}".format([player.level])
-	$Panel/HBoxContainer/Enemies.text = "Defeat Enemies: {0}/{1}".format([$"/root/Main".enemies_defeated, $"/root/Main".enemies_defeated_goal])
--- l.gd
+++
@@ -1,4 +0,0 @@
-extends OmniLight3D
-
-func _process(delta) -> void:
-	rotate_y(1.0*delta)
